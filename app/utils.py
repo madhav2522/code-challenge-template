0e59e547c9bbe2d6e0eb726666e3381ad907c034
Madhav Vaddepalli <49142196+madhav2522@users.noreply.github.com>
2025-12-13 00:50:41 +0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,13 @@
+"""Small utility helpers."""
+
+def chunked(iterable, n):
+    """Yield chunks of size n from iterable."""
+    chunk = []
+    for x in iterable:
+        chunk.append(x)
+        if len(chunk) == n:
+            yield chunk
+            chunk = []
+    if chunk:
+        yield chunk
 
